00932592b3c147cbe20b91a7989bc37b46af7a61
bskim <joj1209@naver.com>
2024-07-30 17:02:03 +0900

    dev branch

--- board/urls.py
+++ board/urls.py
@@ -5,5 +5,10 @@ from . import views
 app_name = 'board'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('list/', views.list, name='list'),
+    path('regist/', views.regist, name='regist'),
+    path('read/<int:id>/', views.read, name='read'),
+    path('edit/<int:id>/', views.edit, name='edit'),
+    path('remove/<int:id>/', views.remove, name='remove'),
 ]
