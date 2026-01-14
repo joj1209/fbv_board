c36f4422d8de23c0217b2d738224b3d735993b99
joj1209 <joj1209@naver.com>
2026-01-14 17:41:21 +0900

    table.sql 만들기

---
+++ table.sql
@@ -0,0 +1,33 @@
+BM.프로그램검증현황
+기준일자 vachar2(8) PK1
+DWDM구분코드 vachar2(10) PK2
+타겟테이블명 vachar2(100) PK3
+타겟컬럼명 vachar2(120) PK4
+검증구분 vachar2(2) PK5
+검증유형 vachar2(2) PK6
+차수 vachar2(2) PK7
+검증내용 vachar2(50)
+검증프로그램ID vachar2(30)
+파라미터값 vachar2(100)
+검증결과구분 number(5)
+검증결과값 clob
+검증모수 number(19)
+검증시작일시 timestamp(6)
+적재일시 timestamp(6)
+
+
+20260101
+DM
+~
+BI메타검증_테이블단위
+03
+04
+01
+테이블메타검증
+MCSBM_008
+{null}
+0
+BI메타검증결과 테이블 정상매핑됨
+1
+시간~
+시간~
